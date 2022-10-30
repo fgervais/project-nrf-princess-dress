22f330e39b512537b9be024ac2fcd1e91a870453
Francois Gervais <francoisgervais@gmail.com>
2022-10-29 20:17:55 -0400

    Add button and PM

--- app/src/main.c
+++ app/src/main.c
@@ -1,25 +1,34 @@
-#include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/pm/device.h>
+
+#include <app_event_manager.h>
+
+#define MODULE main
+#include <caf/events/module_state_event.h>
+#include <caf/events/button_event.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */
+#define SLEEP_TIME_MS   10
 #define LED0_NODE DT_ALIAS(myled0alias)
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
 
 void main(void)
 {
 	int ret;
+	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+
+	if (app_event_manager_init()) {
+		LOG_ERR("Event manager not initialized");
+	} else {
+		module_set_state(MODULE_STATE_READY);
+	}
 
 	if (!device_is_ready(led.port)) {
 		return;
@@ -30,20 +39,38 @@ void main(void)
 		return;
 	}
 
-	while (1) {
-		LOG_INF("****************************************");
-		LOG_INF("LOOP");
-		LOG_INF("****************************************");
-		printk("Toggle\n");
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return;
-		}
-		k_msleep(SLEEP_TIME_MS);
-	}
+	LOG_INF("****************************************");
+	LOG_INF("MAIN DONE");
+	LOG_INF("****************************************");
+
+	k_sleep(K_SECONDS(3));
+	// pm_device_action_run(cons, PM_DEVICE_ACTION_SUSPEND);
+
+	LOG_INF("PM_DEVICE_ACTION_SUSPEND");
 }
 
+static bool event_handler(const struct app_event_header *eh)
+{
+	int ret;
+	const struct button_event *evt;
 
+	if (is_button_event(eh)) {
+		evt = cast_button_event(eh);
+
+		if (evt->pressed) {
+			LOG_INF("Pin Toggle");
+			ret = gpio_pin_toggle_dt(&led);
+			if (ret < 0) {
+				return;
+			}
+		}
+	}
+
+	return true;
+}
+
+APP_EVENT_LISTENER(MODULE, event_handler);
+APP_EVENT_SUBSCRIBE(MODULE, button_event);
 
 
 
@@ -401,21 +428,3 @@ void main(void)
 
 // 	LOG_INF("PM_DEVICE_ACTION_SUSPEND");
 // }
-
-// static bool event_handler(const struct app_event_header *eh)
-// {
-// 	const struct button_event *evt;
-
-// 	if (is_button_event(eh)) {
-// 		evt = cast_button_event(eh);
-
-// 		if (evt->pressed) {
-// 			publisher();
-// 		}
-// 	}
-
-// 	return true;
-// }
-
-// APP_EVENT_LISTENER(MODULE, event_handler);
-// APP_EVENT_SUBSCRIBE(MODULE, button_event);

d5727c87043b62a66c6a30f3f1a5f537679ffe52
Francois Gervais <francoisgervais@gmail.com>
2023-03-22 18:53:57 -0400

    Add the APA102 test code

--- app/src/main.c
+++ app/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/led_strip.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 #include <zephyr/pm/device.h>
@@ -17,8 +18,25 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #define LED0_NODE DT_ALIAS(myled0alias)
 
+#define STRIP_NUM_LEDS 20
+
+#define DELAY_TIME K_MSEC(40)
+
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+static const struct led_rgb colors[] = {
+	{ .r = 0xff, .g = 0x00, .b = 0x00, }, /* red */
+	{ .r = 0x00, .g = 0xff, .b = 0x00, }, /* green */
+	{ .r = 0x00, .g = 0x00, .b = 0xff, }, /* blue */
+};
+
+static const struct led_rgb black = {
+	.r = 0x00,
+	.g = 0x00,
+	.b = 0x00,
+};
+
+struct led_rgb strip_colors[STRIP_NUM_LEDS];
 
 static int ism330dhcx_acc_trig_cnt;
 static int ism330dhcx_gyr_trig_cnt;
@@ -89,6 +107,18 @@ static void ism330dhcx_config(const struct device *ism330dhcx)
 	sensor_trigger_set(ism330dhcx, &trig, ism330dhcx_gyr_trigger_handler);
 }
 
+const struct led_rgb *color_at(size_t time, size_t i)
+{
+	size_t rgb_start = time % STRIP_NUM_LEDS;
+
+	if (rgb_start <= i && i < rgb_start + ARRAY_SIZE(colors)) {
+		return &colors[i - rgb_start];
+	} else {
+		return &black;
+	}
+}
+
+
 
 void main(void)
 {
@@ -97,9 +127,18 @@ void main(void)
 	const struct device *const cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 	const struct device *const ism330dhcx = DEVICE_DT_GET_ONE(st_ism330dhcx);
 
+	const struct device *strip = DEVICE_DT_GET_ANY(apa_apa102);
+
 	struct sensor_value accel1[3];
 	struct sensor_value gyro[3];
 	int cnt = 0;
+
+	size_t i, time;
+
+	// while (1) {
+	// 	// LOG_INF("MAIN DONE");
+	// 	k_sleep(K_SECONDS(1));
+	// }
 
 	if (app_event_manager_init()) {
 		LOG_ERR("Event manager not initialized");
@@ -116,12 +155,42 @@ void main(void)
 		return;
 	}
 
-	if (!device_is_ready(ism330dhcx)) {
-		LOG_ERR("%s: device not ready.", ism330dhcx->name);
+	// if (!device_is_ready(ism330dhcx)) {
+	// 	LOG_ERR("%s: device not ready.", ism330dhcx->name);
+	// 	return;
+	// }
+
+	// ism330dhcx_config(ism330dhcx);
+
+	if (!strip) {
+		LOG_ERR("LED strip device not found");
 		return;
+	} else if (!device_is_ready(strip)) {
+		LOG_ERR("LED strip device %s is not ready", strip->name);
+		return;
+	} else {
+		LOG_INF("Found LED strip device %s", strip->name);
 	}
 
-	ism330dhcx_config(ism330dhcx);
+
+	LOG_INF("Displaying pattern on strip");
+	time = 0;
+	while (1) {
+		for (i = 0; i < STRIP_NUM_LEDS; i++) {
+			memcpy(&strip_colors[i], color_at(time, i),
+			       sizeof(strip_colors[i]));
+		}
+		led_strip_update_rgb(strip, strip_colors, STRIP_NUM_LEDS);
+		k_sleep(DELAY_TIME);
+		time++;
+	}
+
+
+	// while (1) {
+	// 	LOG_INF("MAIN DONE");
+	// 	k_sleep(K_SECONDS(1));
+	// }
+
 
 	while (1) {
 		sensor_channel_get(ism330dhcx, SENSOR_CHAN_ACCEL_XYZ, accel1);
